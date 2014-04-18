78d9e312ba96dd12e85234635f220d11031c74b2
Mahesh Yellai <mahesh.yellai@gmail.com>
2014-04-18 10:23:47 +0530

    Bumped version

--- muppet/__init__.py
+++ muppet/__init__.py
@@ -1,7 +1,7 @@
 from muppet.durable_channel import DurableChannel
 from muppet.remote_channel import RemoteChannel
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 VERSION = tuple(map(int, __version__.split('.')))
 
 __all__ = [
